525141a4180c11d89cbe08873a3f2a114efb39f6
ummsehun <yacsis378@gmail.com>
2026-04-15 23:49:55 +0900

    feat:typescripts 6.0 upgrade

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,11 @@ import react from "@vitejs/plugin-react"
 export default defineConfig({
   plugins: [react()],
   resolve: {
+    dedupe: ["react", "react-dom"],
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      react: path.resolve(__dirname, "./node_modules/react"),
+      "react-dom": path.resolve(__dirname, "./node_modules/react-dom"),
     },
   },
 })
